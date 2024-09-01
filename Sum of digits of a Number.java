e18559eafa2ca32564b4539f440213e0d0ee34ab
Siddhi Sakharkar23 <130655566+Siddhisakharkar@users.noreply.github.com>
2024-09-01 09:29:12 +0530

    Create Sum of digits of a Number.java

---
+++ Sum of digits of a Number.java
@@ -0,0 +1,15 @@
+class Main
+{
+    public static void main(String args[])
+    {
+        int num = 1234;
+        int sum = 0;
+        
+        while(num != 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println(sum);
+    }
+}
