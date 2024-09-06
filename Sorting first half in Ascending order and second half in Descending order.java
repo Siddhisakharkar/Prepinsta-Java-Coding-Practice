d6294a1071c9f1f542710e0187181eec9510c400
Siddhi Sakharkar23 <130655566+Siddhisakharkar@users.noreply.github.com>
2024-09-06 19:15:03 +0530

    Create Sorting first half in Ascending order and second half in Descending order.java

---
+++ Sorting first half in Ascending order and second half in Descending order.java
@@ -0,0 +1,37 @@
+class Main
+{
+    static void printOrder(int a[], int n)
+    {
+        int temp;
+        for(int i = 0;i<n-1;i++)
+        {
+            for(int j = 0;j<n/2;j++)
+            {
+                if(a[j] > a[j +1])
+                {
+                    temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+            for(int j = n/2; j<n-1;j++)
+            {
+                if(a[j] < a[j+1])
+                {
+                    temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+        for(int i = 0; i<n ; i++)
+        System.out.println(a[i]+" ");
+        
+    }
+    public static void main(String args[])
+    {
+        int[] arr = {3,4,2,1,30,20,50,10};
+        int n = arr.length;
+        printOrder(arr , n);
+    }
+}
