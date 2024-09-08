26319d1b920dda86fa00a6d2c4d8204ccce9e2a8
Siddhi Sakharkar23 <130655566+Siddhisakharkar@users.noreply.github.com>
2024-09-08 18:54:44 +0530

    Create character is a Vowel or Consonant.java

---
+++ character is a Vowel or Consonant.java
@@ -0,0 +1,14 @@
+class Main
+{
+    public static void main(String args[])
+    {
+        char c = 'g';
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+        {
+            System.out.println("Is a vowel");
+        }
+        else{
+            System.out.println("Is a consonant");
+        }
+    }
+}
