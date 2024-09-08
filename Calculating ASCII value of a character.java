65dc8e515249dc9fdb22a7ba74cb452f60e932f8
Siddhi Sakharkar23 <130655566+Siddhisakharkar@users.noreply.github.com>
2024-09-08 19:01:51 +0530

    Create Calculating ASCII value of a character.java

---
+++ Calculating ASCII value of a character.java
@@ -0,0 +1,10 @@
+class Main
+{
+    public static void main(String args[])
+    {
+        char c='A';
+        int i = c;
+        
+        System.out.println("The ASCII Value of"+ c +" is"+ i);
+    }
+}
