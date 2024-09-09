622bf039f3a6623ec2f67dced2c2d76ecc81c2dd
Siddhi Sakharkar23 <130655566+Siddhisakharkar@users.noreply.github.com>
2024-09-09 14:09:06 +0530

    Create find length of the string without using length function.java

---
+++ find length of the string without using length function.java
@@ -0,0 +1,12 @@
+class Main
+{
+    public static void main(String args[])
+    {
+        int length = 0;
+        String s = "Prepinsta";
+        
+        for(char c1: s.toCharArray())
+        length++;
+        System.out.println(length);
+    }
+}
