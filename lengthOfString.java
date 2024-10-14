0a80b84e3b61d40a0fe049dda4975300bc4e6469
Siddhi Sakharkar23 <130655566+Siddhisakharkar@users.noreply.github.com>
2024-10-14 07:32:49 +0530

    Create lengthOfString.java

---
+++ lengthOfString.java
@@ -0,0 +1,14 @@
+class Main{
+    public static void main(String args[])
+    {
+        int length =  0;
+        String s = "Alphabet";
+        for(char c1 : s.toCharArray())
+        {
+            length++;
+           
+        }
+        System.out.println("The length of the string is"+length);
+        
+    }
+}
