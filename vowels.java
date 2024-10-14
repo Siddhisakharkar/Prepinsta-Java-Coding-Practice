6f6102b12ae4e6a68c4eb13c6abcd6f229845bd7
Siddhi Sakharkar23 <130655566+Siddhisakharkar@users.noreply.github.com>
2024-10-14 07:21:23 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main(String args[])
+    {
+        char c = 'g';
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+        {
+            System.out.println("The given character is vowels ");
+        }
+        else{
+            System.out.println("The given character is not vowels");
+        }
+    }
+}
