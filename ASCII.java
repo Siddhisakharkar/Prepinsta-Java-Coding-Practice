55bcf2a38975f5b4a8a5828036a8499bbea90163
Siddhi Sakharkar23 <130655566+Siddhisakharkar@users.noreply.github.com>
2024-10-14 07:28:11 +0530

    Create ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String args[])
+    {
+        char c = 'A';
+        int i = c;
+        
+        System.out.println("The Ascii value of given character "+c+" is"+i);
+    }
+}
