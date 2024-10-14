7a387bceb07d5ce9149da8e9cc15f9c6ba93c246
Siddhi Sakharkar23 <130655566+Siddhisakharkar@users.noreply.github.com>
2024-10-14 07:25:13 +0530

    Create alphabet or character.java

---
+++ alphabet or character.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main(String args[])
+    {
+        char c = '9';
+        if(c >='a' && c<='z'||c >='A' && c <='Z')
+        {
+            System.out.println("The given character is an alphabet");
+        }
+        else{
+            System.out.println("The given character is not an albhabet");
+        }
+    }
+}
